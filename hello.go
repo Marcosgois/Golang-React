2e9135f3f68a7451da4ed7bb63a57d5670224bc5
Marcos Gois <goisinho_5@hotmail.com>
2021-06-17 10:44:17 -0300

    Post and Get working (API)

--- hello.go
+++ hello.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"sync"
+	"time"
 )
 
 type Twitter struct {
+	ID      string
 	User    string
 	Message string
 	Data    string
-	ID      string
+	Time    time.Time
 }
 
 type twitterHandlers struct {
+	sync.Mutex
 	store map[string]Twitter
 }
 
@@ -32,12 +38,13 @@ func (h *twitterHandlers) twitters(w http.ResponseWriter, r *http.Request) {
 
 func (h *twitterHandlers) get(w http.ResponseWriter, r *http.Request) {
 	twitters := make([]Twitter, len(h.store))
-
+	h.Lock()
 	i := 0
 	for _, twitter := range h.store {
 		twitters[i] = twitter
 		i++
 	}
+	h.Unlock()
 
 	jsonBytes, err := json.Marshal(twitters)
 	if err != nil {
@@ -49,25 +56,49 @@ func (h *twitterHandlers) get(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsonBytes)
 }
+
 func (h *twitterHandlers) post(w http.ResponseWriter, r *http.Request) {
 
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	ct := r.Header.Get("content-type")
+	if ct != "application/json" {
+		w.WriteHeader(http.StatusUnsupportedMediaType)
+		w.Write([]byte(fmt.Sprintf("Need content-type 'application/json', but got '%s'", ct)))
+		return
+	}
+
+	var twitter Twitter
+	err = json.Unmarshal(bodyBytes, &twitter)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+	}
+
+	twitter.Time = time.Now()
+
+	h.Lock()
+	h.store[twitter.ID] = twitter
+	w.Write([]byte(fmt.Sprintf("Postando ID: '%s'", twitter.ID)))
+	defer h.Unlock()
 }
+
 func newTwitterHandlers() *twitterHandlers {
 	return &twitterHandlers{
-		store: map[string]Twitter{
-			"id1": {
-				User:    "Marcos Gois",
-				Message: "Hello World",
-				Data:    "16/06/2021",
-				ID:      "id1",
-			},
-		},
+		store: map[string]Twitter{},
 	}
 }
 
 func main() {
 	twitterHandlers := newTwitterHandlers()
-	http.HandleFunc("/twitter", twitterHandlers.get)
+	http.HandleFunc("/twitters", twitterHandlers.twitters)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
